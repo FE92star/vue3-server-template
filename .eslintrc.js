b439e0e8b4433c04b0aca91e40580dcfdc6f36f6
maobao <maobao@duiba.com.cn>
2022-09-13 20:03:30 +0800

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+const { defineConfig } = require('eslint-define-config')
+
+module.exports = defineConfig({
+  root: true,
+  extends: ['@fe92star/eslint-config-basic', '@fe92star/eslint-config-prettier'],
+})
