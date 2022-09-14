9b44e50772ce44b38e57875f1937302006328a7f
FE92star <1737765939@qq.com>
2022-09-14 11:40:51 +0800

    feat(server): 新增koa app应用启动逻辑

--- server/app.js
+++ server/app.js
@@ -1,5 +1,21 @@
 const Koa = require('koa')
+const cors = require('koa-cors')
+const bodyParser = require('koa-bodyparser')
 
 const app = new Koa()
 
-module.exports = app
+// 开启cors跨域
+app.use(
+  cors({
+    origin: '*',
+    allowMethods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Accept']
+  })
+)
+
+// 上下文解析
+app.use(bodyParser())
+
+app.listen(3000)
+
+console.warn('koa 应用启动成功')
